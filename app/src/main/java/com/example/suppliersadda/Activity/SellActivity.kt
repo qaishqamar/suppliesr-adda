2fa0beb9578a0f319c7cc53baf7d5dcdbfee3164
Sandeep kumar <sandeepkarmkar49@gmail.com>
2021-02-25 23:26:04 +0530

    XML step loader working :)

--- app/src/main/java/com/example/suppliersadda/Activity/SellActivity.kt
+++ app/src/main/java/com/example/suppliersadda/Activity/SellActivity.kt
@@ -4,19 +4,23 @@ import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewStub
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 import com.example.suppliersadda.R
 import com.example.suppliersadda.utils.ViewAnimation
+import kotlinx.android.synthetic.main.activity_sell.*
 
 class SellActivity : AppCompatActivity() {
 
-    private val MAX_STEP = 6
+    private val MAX_STEP = 5
     private var current_step = 1
     private var status: TextView? = null
+    private var stubs:ViewStub? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,12 +42,32 @@ class SellActivity : AppCompatActivity() {
 
     private fun initComponent() {
         status = findViewById<View>(R.id.status) as TextView
+
+        //Defining view sub
+//        stub = findViewById<View>(R.id.layout_step) as ViewStub;
+//        stub!!.layoutResource(R.layout.sell_step1);
+
+//        stubs = findViewById(R.id.layout_step) as ViewStub
+//        stubs!!.layoutResource = R.layout.sell_step1
+//        stubs!!.inflate()
+
+        //Trying old way of including xml's
+        var step1 = findViewById<View>(R.id.step1__)
+        var step2 = findViewById<View>(R.id.step2__)
+        var step3 = findViewById<View>(R.id.step3__)
+        var step4 = findViewById<View>(R.id.step4__)
+        var step5 = findViewById<View>(R.id.step5__)
+        stepLoader(current_step)
+
+
         (findViewById<View>(R.id.lyt_back) as LinearLayout).setOnClickListener {
             backStep(current_step)
+            stepLoader(current_step)
             bottomProgressDots(current_step)
         }
         (findViewById<View>(R.id.lyt_next) as LinearLayout).setOnClickListener {
             nextStep(current_step)
+            stepLoader(current_step)
             bottomProgressDots(current_step)
         }
         val str_progress = String.format(
@@ -100,10 +124,65 @@ class SellActivity : AppCompatActivity() {
             dots[i]!!.setColorFilter(resources.getColor(R.color.grey_20), PorterDuff.Mode.SRC_IN)
             dotsLayout.addView(dots[i])
         }
-        if (dots.size > 0) {
+        if (dots.size > 0)
+        {
             dots[current_index]!!.setImageResource(R.drawable.shape_circle)
             dots[current_index]!!
                 .setColorFilter(resources.getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
         }
     }
+
+    private fun stepLoader(current_position: Int){
+//        if (current_position == 1 && R.layout.sell_step_loading!=null){
+//            stubs!!.layoutResource = R.layout.sell_step1
+//            stubs!!.inflate()
+//        }
+//        else if (current_position == 2 && R.layout.sell_step_loading!=null){
+//            stubs!!.layoutResource = R.layout.sell_step2
+//            stubs!!.inflate()
+//        }else{
+//            stubs!!.layoutResource = R.layout.sell_step_loading
+//            stubs!!.inflate()
+//        }
+
+        if(current_position == 1){
+            step1__.visibility = View.VISIBLE
+            step2__.visibility = View.INVISIBLE
+            step3__.visibility = View.INVISIBLE
+            step4__.visibility = View.INVISIBLE
+            step5__.visibility = View.INVISIBLE
+        }
+        else if(current_position == 2){
+            step1__.visibility = View.INVISIBLE
+            step2__.visibility = View.VISIBLE
+            step3__.visibility = View.INVISIBLE
+            step4__.visibility = View.INVISIBLE
+            step5__.visibility = View.INVISIBLE
+        }
+        else if(current_position == 3){
+            step1__.visibility = View.INVISIBLE
+            step2__.visibility = View.INVISIBLE
+            step3__.visibility = View.VISIBLE
+            step4__.visibility = View.INVISIBLE
+            step5__.visibility = View.INVISIBLE
+        }
+        else if(current_position == 4){
+            step1__.visibility = View.INVISIBLE
+            step2__.visibility = View.INVISIBLE
+            step3__.visibility = View.INVISIBLE
+            step4__.visibility = View.VISIBLE
+            step5__.visibility = View.INVISIBLE
+        }
+        else if(current_position == 5){
+            step1__.visibility = View.INVISIBLE
+            step2__.visibility = View.INVISIBLE
+            step3__.visibility = View.INVISIBLE
+            step4__.visibility = View.INVISIBLE
+            step5__.visibility = View.VISIBLE
+        }
+
+
+
+    }
+
 }
